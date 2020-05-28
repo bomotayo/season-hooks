b48522e66f7f89f7a216d102d94cf19319ed987b
bomotayo <omotayobabatundee@gmail.com>
2020-05-28 10:53:54 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay';
+import Loader from './Loader';
+import useLocation from './useLocation';
+
+const App = () => {
+
+  const [lat, errorMessage] = useLocation();
+  
+  let content;
+
+  if(errorMessage){
+    content = <div>Error: {errorMessage} </div>
+  }
+  else if(lat){
+    content = <SeasonDisplay lat={lat}/>
+  }
+  else{
+    content = <Loader message="Please accept location request"/>
+  }
+
+  return  <div className="border red">{content}</div>
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  <App/>,
+  document.querySelector('#root')
+)
